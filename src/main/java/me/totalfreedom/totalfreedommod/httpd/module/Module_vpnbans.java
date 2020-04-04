d1f9ee8860a38cf31fdfdbb09624ba286ad6dd05
Ivan <ivanmorgan43@gmail.com>
2020-04-04 02:57:11 -0400

    bruh

---
+++ src/main/java/me/totalfreedom/totalfreedommod/httpd/module/Module_vpnbans.java
@@ -0,0 +1,49 @@
+package me.totalfreedom.totalfreedommod.httpd.module;
+
+import java.io.File;
+import me.totalfreedom.totalfreedommod.TotalFreedomMod;
+import me.totalfreedom.totalfreedommod.admin.Admin;
+import me.totalfreedom.totalfreedommod.banning.PermbanList;
+import me.totalfreedom.totalfreedommod.banning.VPNBanList;
+import me.totalfreedom.totalfreedommod.httpd.HTTPDaemon;
+import me.totalfreedom.totalfreedommod.httpd.NanoHTTPD;
+
+public class Module_vpnbans extends HTTPDModule
+{
+
+    public Module_vpnbans(TotalFreedomMod plugin, NanoHTTPD.HTTPSession session)
+    {
+        super(plugin, session);
+    }
+
+    @Override
+    public NanoHTTPD.Response getResponse()
+    {
+        File permbanFile = new File(plugin.getDataFolder(), VPNBanList.CONFIG_FILENAME);
+
+        // if people get mad that opening the permban list to the public causes a mass DDoS attack on the server due to ip
+        // leaks, it wasnt my idea
+
+        /*final String remoteAddress = socket.getInetAddress().getHostAddress();
+        if (!isAuthorized(remoteAddress))
+        {
+            return new NanoHTTPD.Response(NanoHTTPD.Response.Status.NOT_FOUND, NanoHTTPD.MIME_PLAINTEXT,
+                    "You may not view the permban list. Your IP, " + remoteAddress + ", is not registered to an admin on the server.");
+        }*/
+        if (permbanFile.exists())
+        {
+            return HTTPDaemon.serveFileBasic(new File(plugin.getDataFolder(), VPNBanList.CONFIG_FILENAME));
+        }
+        else
+        {
+            return new NanoHTTPD.Response(NanoHTTPD.Response.Status.NOT_FOUND, NanoHTTPD.MIME_PLAINTEXT,
+                    "Error 404: Not Found - The requested resource was not found on this server.");
+        }
+    }
+
+    private boolean isAuthorized(String remoteAddress)
+    {
+        Admin entry = plugin.al.getEntryByIp(remoteAddress);
+        return entry != null && entry.isActive();
+    }
+}
